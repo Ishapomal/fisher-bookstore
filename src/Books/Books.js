c94eba8f8a1fc683cc5013c64c3292aee09bfb48
Ishapomal <ishapomal@gmail.com>
2020-02-19 17:52:08 -0500

    add book cards (#21)

--- src/Books/Books.js
+++ src/Books/Books.js
@@ -1,13 +1,65 @@
-import React from "react";
-import "./Books.css";
+import React,{ Component } from "react";import "./Books.css";
+import { BookDisplay } from "./BookDisplay";
 
-export default function Books() {
+
+ export default class Books extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            books: [
+                {
+                    id: 1,
+                    title: "Domain Driven Design",
+                    author: "Eric Evans",
+                    isbn: "978-0321125217"
+                },
+                {
+                    id: 2,
+                    title: "Accelerate",
+                    author: "Nicole Forsgren",
+                    isbn: "978-1942788331" 
+                },
+                {
+                    id: 3,
+                    title: "Accelerate",
+                    author: "Nicole Forsgren",
+                    isbn: "978-1942788331" 
+                },
+                {
+                    id: 4,
+                    title: "Accelerate",
+                    author: "Nicole Forsgren",
+                    isbn: "978-1942788331" 
+                },
+                {
+                    id: 5,
+                    title: "Accelerate",
+                    author: "Nicole Forsgren",
+                    isbn: "978-1942788331" 
+                },
+                {
+                    id: 6,
+                    title: "Accelerate",
+                    author: "Nicole Forsgren",
+                    isbn: "978-1942788331" 
+                },
+
+            ]
+        };
+    };
+    render(){
     return (
         <div className = "Books">
             <div className= "lander">
-                <h1> Books </h1>
-                <p> Welcome to Fisher Bookstore.</p>
+              <BookDisplay books={this.state.books}/>
             </div>
         </div>
     );
-}
+    }
+    
+    
+    }
+
+
+    
+    
