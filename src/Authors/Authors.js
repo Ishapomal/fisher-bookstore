ee2552662fdc211a9ad01def31fba659c7fb2d3e
Isha Pomal <ishapomal@gmail.com>
2020-03-27 21:33:27 -0500

    changes

--- src/Authors/Authors.js
+++ src/Authors/Authors.js
@@ -1,47 +1,21 @@
-import React,{ Component } from "react";
-import "./Authors.css";
+import React,{ useEffect, useState } from "react";
 import { AuthorDisplay } from "./AuthorDisplay";
 
-export default class Authors extends Component{
-constructor(props){
-    super(props);
-    this.state = {
-        authors: [
-            {
-                fName: "FirstName1",
-                lName: "lol"
-            },
-            {
-                fName: "FirstName2",
-                lName: "lol"
-            },
-            {
-                fName: "FirstName3",
-                lName: "lolol"
-            },
-            {
-                fName: "FirstName4",
-                lName: "lololol"
-            },
-            {
-                fName: "FirstName5",
-                lName: "lololol0l"
-            },
-            {
-                fName: "FirstName6",
-                lName: "lolololololol"
-            },
-   
-        ]
-    };
-};
-render(){
-    return (
-        <div className = "Authors">
-            <div className= "lander">
-              <AuthorDisplay authors={this.state.authors}/>
-            </div>
+
+ export default function Authors(props){
+   const [ data, setData] = useState([]);
+
+   useEffect(() => {
+       fetch("https://localhost:5001/api/authors/")
+       .then(response => response.json())
+       .then(data => setData(data));
+   }, []);
+   return (
+       <div className = "Authors">
+        <div className = "lander">
+        <AuthorDisplay authors = {data} />
         </div>
-    );
-    }
-}
+       </div>
+   );
+ }
+       
